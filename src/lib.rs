0adfbcb0e12d45c8a2ba305ffd3bd7936ce8ecee
nallux-dozryl <nallux-dozryl@protonmail.com>
2025-09-04 22:53:27 +0700

    reexport

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,10 @@ pub use transaction_types::*;
 // Re-export block types for RPC usage
 pub use block_types::{BlockPage, SimpleTransaction, SimpleTransactionInput, SimpleTransactionOutput, CoinbaseRecipient};
 
+// Nockchain reexport
+pub use nockapp;
+pub use nockvm;
+
 // Test modules
 #[cfg(test)]
 mod tests;
