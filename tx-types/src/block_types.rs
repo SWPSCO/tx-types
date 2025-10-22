f137da4c75536999d39e8a8996f406246e69a8f4
nallux-dozryl <nallux-dozryl@protonmail.com>
2025-10-23 05:03:25 +0700

    reenable proof bytes

--- tx-types/src/block_types.rs
+++ tx-types/src/block_types.rs
@@ -370,13 +370,17 @@ pub struct Pow {
 
 impl NounDecode for Pow {
     fn from_noun(noun: &Noun) -> Result<Self, NounDecodeError> {
-        /*
+        if noun.is_atom() {
+            return Ok(Pow { p: None })
+        }
+        let proof = noun.as_cell()
+            .map_err(|_| NounDecodeError::ExpectedCell)?
+            .tail();
+
         let mut slab: NounSlab = NounSlab::new();
-        slab.copy_into(*noun);
+        slab.copy_into(proof);
         let noun_bytes: Bytes = slab.jam();
         Ok(Pow { p: Some(noun_bytes) })
-        */
-        Ok(Pow { p: None })
     }
 }
 
