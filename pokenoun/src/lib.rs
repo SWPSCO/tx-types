846eba8e0238dd8d6f525e721583e4ed4bcb23e4
reid <reid@scogg.in>
2025-12-13 14:09:32 -0600

    add zset

--- pokenoun/src/lib.rs
+++ pokenoun/src/lib.rs
@@ -9,9 +9,10 @@ mod arena;
 mod jam;
 mod tip5;
 mod zmap;
+mod zset;
 
 pub use arena::{Arena, AtomId, Cell, CellId, Noun};
 pub use jam::{cue, jam, CodecError};
 pub use tip5::{hash_noun_varlen, hash_ten_cell, Tip5Error, GOLDILOCKS_P};
 pub use zmap::{canonical_zmap_put, ZMapError};
-
+pub use zset::{canonical_zset_put, ZSetError};
